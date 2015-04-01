aff814567138be0ce4e29d25ca32f3986792868b
Guriev Nicholas <guriev-ns@ya.ru>
2015-04-02 00:53:53 +0300

    Сложение и умножение

--- reverse-polish/ariphmetic.c
+++ reverse-polish/ariphmetic.c
@@ -1,8 +1,26 @@
+#include <stddef.h>
 #include <string.h>
 
 #include "ariphmetic.h"
 
 #define N 30
+
+void normalize(char* num)
+{
+	char* nv = NULL;
+
+	while (num && *num)
+	{
+		if (*num == '0')
+			nv = num;
+		else
+			nv = NULL;
+		num++;
+	}
+
+	if (nv)
+		*nv = '\0';	/* NOTE: нулевой символ, а не цифра ноль */
+}
 
 void grv_arph_add(char* a, char* b)
 {
@@ -25,8 +43,39 @@ void grv_arph_add(char* a, char* b)
 	a[i] = '\0';	/* NOTE: нулевой символ, а не цифра ноль */
 }
 
-void grv_arph_substract(char* a, char* b) {}
+void grv_arph_substract(char* a, char* b)
+{
+    const size_t l1 = strlen(a);
+	const size_t l2 = strlen(b);
+	int below = 0;
+	size_t i;
 
-void grv_arph_produt(char* a, char* b) {}
+	for (i = 0; i < l1 || i < l2; i++)
+	{
+		/* копипаста из выше */
+		int d = (i < l1 ? a[i] - '0' : 0) - (i < l2 ? b[i] - '0' : 0) - below;
+		if (below = d < 0)
+			d += 10;
+		a[i] = d % 10 + '0';
+	}
+
+	/* на этот момент i указыват на конец первого числа */
+	if (below);
+	/* TODO: сигнализирование об ошибке */
+	normalize(a);
+}
+
+void grv_arph_produt(char* a, char* b)
+{
+	char c[N], d[N];
+
+	strcpy(c, a);
+	strcpy(d, b);
+	while (strcmp(d, "1"))
+	{
+		grv_arph_add(a, c);
+		grv_arph_substract(d, "1");
+	}
+}
 
 void grv_arph_negative(char* a) {}
